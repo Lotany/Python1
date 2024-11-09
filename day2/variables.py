746d1726d580be68ab823ebcfdf509c4bba8324a
lotany <ablekilinda@gmail.com>
2024-11-09 16:13:24 +0300

    variable declaration

---
+++ day2/variables.py
@@ -0,0 +1,14 @@
+#day 2 of 30 days of programming
+first_name = "lotan"
+last_name = "mtoto"
+full_name = 'Lotan Mtot'
+country_name ="Lotan"
+city = "Mombasa"
+age = 5
+year =2024
+is_Married = "Yes"
+is_true =True
+is_light = "yellow"
+
+blue_Color, Pink_Color, Red_Color ="Yes"
+
