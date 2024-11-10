f56943bf08075bdcfa3b06936b2cb4e82d853dd3
lotany <ablekilinda@gmail.com>
2024-11-10 05:20:59 +0300

    Area of a triangle

---
+++ day2/operator.py
@@ -0,0 +1,10 @@
+my_Age = 26
+my_Height = 5.8
+my_Var= (2 + 1)
+
+print('Hello welcome to My Area game Pookie:.')
+base_TRi=float(input("Enter the base of your traingle: "))
+height_Triangle= float(input("Enter the height of the triangle:."))
+area_tri=0.5*base_TRi*height_Triangle
+print("The area of a triangle is:",area_tri)
+
