cc9594d82b1ed2e3a8c9e477e3ffc45c091dd884
lotany <ablekilinda@gmail.com>
2024-11-11 15:59:18 +0300

    unpacking items from list

--- day5/list.py
+++ day5/list.py
@@ -13,6 +13,15 @@ print(last_Fruit)
 nega_Tive = fruits[-1]
 print(nega_Tive)
 
+#unpacking list items using *rest
 fruits=['banana', 'orange', 'mango','lemon','lime','apple']
 fruit1,fruit2,fruit3, *rest =fruits
 print(rest)
+
+first, second, third, *rest, tenth =[1,2,3,4,5,6,7,8,9,10]
+print(rest)
+
+countries = ['Germany', 'France','Belgium', 'Sweden','Denmark','Finland', 'Norway','Iceland','Estonia']
+gr,fr,bg,sw, *scan, es =countries
+print(gr)
+print(scan)
