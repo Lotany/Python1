9f344cbc5b715870bc27ab41981ca5b55d5140ed
lotany <ablekilinda@gmail.com>
2024-11-12 08:04:07 +0300

    adding items to a lit

--- day5/list.py
+++ day5/list.py
@@ -25,3 +25,23 @@ countries = ['Germany', 'France','Belgium', 'Sweden','Denmark','Finland', 'Norwa
 gr,fr,bg,sw, *scan, es =countries
 print(gr)
 print(scan)
+
+#slicing list items from a list
+fruits=['banana', 'orange', 'mango','lemon','lime','apple']
+all_Fruits = fruits[0:4]
+print('*********************')
+print(all_Fruits)
+lemo = fruits[::7]
+print("@@@@@@@@@@@")
+print(lemo)
+
+#modifying list
+fruits=['banana', 'orange', 'mango','lemon','lime','apple']
+fruits[-1] = "Lotan"
+print(fruits)
+does_exist = 'banana' in fruits
+print(does_exist)
+
+fruits=['banana', 'orange', 'mango','lemon','lime','apple']
+mendy =fruits.append('kilifi')
+print(mendy)
