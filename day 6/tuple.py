7d4d361b2193f373769ba3b2f4b96c0bbaa4a0e8
lotany <ablekilinda@gmail.com>
2024-11-14 15:20:15 +0300

    indexing

---
+++ day 6/tuple.py
@@ -0,0 +1,11 @@
+#creating an empty tuple
+tpl =('item 1', 'item 2', 'item 3')
+fruits =('banana', 'orange', 'mango', 'lemon')
+print(fruits)
+lengthy = len(fruits)
+print(lengthy)
+
+#accesing tuple items using positive and negative indexing
+
+fist_item =fruits[0]
+print(fist_item)
