813912d8a8ef869cd361f63a549fb3b345dbef18
lotany <ablekilinda@gmail.com>
2024-11-14 15:32:39 +0300

    slicing out tuple items

--- day 6/tuple.py
+++ day 6/tuple.py
@@ -6,6 +6,22 @@ lengthy = len(fruits)
 print(lengthy)
 
 #accesing tuple items using positive and negative indexing
-
+#positive indexing
 fist_item =fruits[0]
 print(fist_item)
+last_item = fruits[-1]
+print(last_item)
+print("*******************")
+last_index = len(fruits) -1
+lasty_item = fruits[last_index]
+print(lasty_item)
+print('####################')
+all_fruits = fruits[0-4]
+print(all_fruits)
+all_fruits =fruits [0:]
+print(all_fruits)
+first_fruit =fruits[1:3]
+print(first_fruit)
+
+ferit = fruits[-3]
+print(ferit)
