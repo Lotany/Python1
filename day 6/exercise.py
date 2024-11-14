d15d5e5c19108bdaa74342e28c5f33bc5c0b7634
lotany <ablekilinda@gmail.com>
2024-11-14 16:28:03 +0300

    tuples exercise

--- day 6/exercise.py
+++ day 6/exercise.py
@@ -10,3 +10,18 @@ modify.insert(1,'Juliana')
 
 familly_members = tuple(modify)
 print(familly_members)
+(sibling) =familly_members
+print('$$$$$$$$$$$$$$$$$')
+print(sibling)
+fruits = ('banana','mango')
+vegetables =('kales','Spinach')
+animal_product = ('Meat','eggs','Soup')
+food_stuff_tp = fruits + vegetables + animal_product
+print(food_stuff_tp)
+food_stuff_lt =list(food_stuff_tp)
+sliced =food_stuff_lt[3:-3]
+print(sliced)
+convert =tuple(food_stuff_lt)
+del convert
+del1 = 'Soup' in convert
+print(del1)
