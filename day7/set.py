ce2c0600f460fe7908bd6dd1e743fa694c711c1f
lotany <ablekilinda@gmail.com>
2024-11-15 15:47:06 +0300

    converting list to set

--- day7/set.py
+++ day7/set.py
@@ -1,4 +1,32 @@
 #creating an empty set
 fruits = {'banana','orange','mango','lemon'}
+
+#getting th set length
 lengthy = len(fruits)
 print(lengthy)
+
+#accessing items in a set
+#refer to he loop section
+
+#checking an item using in
+print('Does the set contain orange', 'orange' in fruits)
+
+#adding items to  a set
+fruits.add('Kiwi')
+print(fruits)
+
+#adding multiple items using update
+fruits.update(['tomato','passion','tomoko','apple'])
+print(fruits)
+#removing an item from aset
+fruits.remove('orange')
+print(fruits)
+fruits.pop()
+print(fruits)
+#clearing items from a set
+'''fruits.clear()
+print(fruits)'''
+
+#deleting a set
+del fruits
+print(fruits)
