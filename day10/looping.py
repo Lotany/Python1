51c1e12a2f58e5ce7997056fad1d201776f3bad5
lotany <ablekilinda@gmail.com>
2024-11-17 07:18:33 +0300

    conditions

--- day10/looping.py
+++ day10/looping.py
@@ -1,6 +1,56 @@
+#while loop
 count = 0
 while count < 5:
-    print(count)
+    #print(count)
     count = count + 1
-else:
+    if count ==3:
+        continue
     print(count)
+print("********************************")
+
+#for loop with list
+numbers = [0,1,2,3,4,5]
+for number in numbers:
+    print(number)
+
+#for loop with string
+name = "lotan"
+for n in name:
+    print(n)
+print("############################")
+for i in range(len(name)):
+    print(name[i])
+#for loop with tuple
+numbers = (0,1,2,3,4,5)
+for number in numbers:
+    print(number)
+
+#for loop in dictionary
+
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+}
+for key in person:
+    print(key)
+
+for key, value in person.items():
+    print(key, value)
+
+#range
+lst = list(range(11))
+print(lst)
+
+st =set(range(1,11))
+print(st)
+
+lst =list(range(0,20,3))
+print(lst)
