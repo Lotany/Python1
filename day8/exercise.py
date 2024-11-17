d799c124f66d5fd07c2f58cdde46bdab3d1558e0
lotany <ablekilinda@gmail.com>
2024-11-17 05:43:42 +0300

    dictionsries

---
+++ day8/exercise.py
@@ -0,0 +1,13 @@
+dog = {}
+dog['name'] = 'Putin'
+dog['color'] = 'White'
+dog['breed'] = 'German'
+dog['legs'] = 4
+dog['age'] = 6
+print(dog)
+
+student = {'first_name':'Kilinda', 'last_name':'Mtoto','gender':'male','age':10,'marital_status':'single','skills':'biology','country':'kenya','adress':{'location':'kilifi','box':528}}
+print(len(student))
+print(student.keys())
+print("***********")
+print(student.items())
