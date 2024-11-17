b0ceff08d3a4a368347eee9bcd0b57c61041ddca
lotany <ablekilinda@gmail.com>
2024-11-17 06:26:31 +0300

    conditions

---
+++ day9/numbers.py
@@ -0,0 +1,8 @@
+a = int(input('Enter the fisrt number'))
+b = int(input('enter the second number'))
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("b is smaller than a")
+else:
+    print("a is eqaul to b")
