39ec44d553d2c670624ca2b0a09a82e91c7ce1b9
lotany <ablekilinda@gmail.com>
2024-11-17 06:33:29 +0300

    scores

---
+++ day9/scores.py
@@ -0,0 +1,11 @@
+score =int(input("Enter your score: "))
+if score >= 80 and score <=100:
+    print("A")
+elif score >=70 and score <=79:
+    print("B")
+elif score >=60 and score <=69:
+    print("C")
+elif score >=50 and score <=59:
+    print("D")
+else:
+    print("E")
