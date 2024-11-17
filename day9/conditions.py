ad8dd0db6c33cc0cedfdbb78b70c8d0347e32f7d
lotany <ablekilinda@gmail.com>
2024-11-17 06:07:24 +0300

    conditions

---
+++ day9/conditions.py
@@ -0,0 +1,7 @@
+age= int(input('Enter your age: '))
+min_age =18
+if age >=18:
+    print("you are old enought o drive: ")
+else:
+    print("You need",min_age-age, "more years to drive")
+    print("Goodbye")
