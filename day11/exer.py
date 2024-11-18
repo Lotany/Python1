63b5c16f3b0220c502c72126a9915a5b3b3ca118
lotany <ablekilinda@gmail.com>
2024-11-18 06:02:55 +0300

    is number

---
+++ day11/exer.py
@@ -0,0 +1,21 @@
+def add_two_numbers(num1,num2):
+    sum = num1 + num2
+    return sum
+print(add_two_numbers(10,10))
+
+def area_circle(r):
+    pi =3.14
+    area = pi *r**2
+    return area
+print(area_circle(4))
+
+def add_all_numbers(*args):
+    sum = 0
+    for arg in args:
+        int(sum) +=arg
+        if int(sum) == True:
+            print("Its an integer")
+        else:
+            print("its anumber")
+    return sum
+print(add_all_numbers(2,2,2,2,2))
