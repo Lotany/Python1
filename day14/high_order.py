3faed7effec4205c10b3fbfcacf4cd4b825a6e7c
lotany <ablekilinda@gmail.com>
2024-11-21 05:34:22 +0300

    closures in python

--- day14/high_order.py
+++ day14/high_order.py
@@ -30,3 +30,11 @@ result = high_order_function('cube')
 print(result(3))
 result =high_order_function('absolute')
 print(result(3))
+
+def add_ten():
+    ten =10
+    def add(num):
+        return num + ten
+    return add
+closure_result =add_ten()
+print(closure_result(5))
