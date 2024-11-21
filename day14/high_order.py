9464832d3bf5aa7bd2e01e4091a4fcbbbb6b2636
lotany <ablekilinda@gmail.com>
2024-11-21 05:30:39 +0300

    python closures

--- day14/high_order.py
+++ day14/high_order.py
@@ -7,3 +7,26 @@ def high_order_function(f,lst):
     return summation
 result = high_order_function(sum_numbers,[1,2,3,4,5])
 print(result)
+
+def square(x):
+    return x**2
+def cube(x):
+    return x**3
+def absolute(x):
+    if x >=0:
+        return x
+    else:
+        return -(x)
+def high_order_function(type):
+    if type == 'square':
+        return square
+    elif type =='cube':
+        return cube
+    elif type =='absolute':
+        return absolute
+result =high_order_function('square')
+print(result(3))
+result = high_order_function('cube')
+print(result(3))
+result =high_order_function('absolute')
+print(result(3))
