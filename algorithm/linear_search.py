3246ddf165333f672172023cd160b89161d3629e
lotany <ablekilinda@gmail.com>
2024-11-24 11:13:30 +0300

    linerasearch algorithm in python

---
+++ algorithm/linear_search.py
@@ -0,0 +1,18 @@
+def linear_search(list, target):
+    """
+    returns the index position of the target if found, else retuns none
+    """
+    for i in range(0, len(list)):
+        if list[i] ==target:
+            return i
+    return None
+#verify function if the value is not none it informs the value of the position if not
+#it was not found on the list
+def verify(index):
+    if index is not None:
+        print("Target found at index", index)
+    else:
+        print("Target not found")
+numbers =[1,2,3,4,56,7,8,9,10]
+result =linear_search(numbers,12)
+verify(result)
