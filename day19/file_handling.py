eb4b8225e47f770535d4fa8e02dc205f82b5bafd
lotany <ablekilinda@gmail.com>
2024-11-25 05:57:22 +0300

    text extensions

--- day19/file_handling.py
+++ day19/file_handling.py
@@ -8,6 +8,12 @@ line = f1.readlines()
 print(line)
 f1.close()
 
+
+
+#appending a new text
+with open('./files/reading_file_example.txt','a') as g:
+    g.write('This text has to be appended')
+
 with open('./files/reading_file_example.txt') as f:
     lines =f.read()
     print(lines)
