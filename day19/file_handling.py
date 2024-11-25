7102be3aa95224cce6351bd04d43d1ddecffb615
lotany <ablekilinda@gmail.com>
2024-11-25 06:21:36 +0300

    python modules

--- day19/file_handling.py
+++ day19/file_handling.py
@@ -17,3 +17,21 @@ with open('./files/reading_file_example.txt','a') as g:
 with open('./files/reading_file_example.txt') as f:
     lines =f.read()
     print(lines)
+
+import webbrowser
+url_lists =[
+    'http://www.python.org',
+    'http://github.com/lotany',]
+
+for url in url_lists:
+    webbrowser.open_new_tab(url)
+
+import requests # importing the request module
+
+url = 'https://www.w3.org/TR/PNG/iso_8859-1.txt' # text from a website
+
+response = requests.get(url) # opening a network and fetching a data
+print(response)
+print(response.status_code) # status code, success:200
+print(response.headers)     # headers information
+print(response.text) # gives all the text from the page
