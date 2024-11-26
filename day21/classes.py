dcd60290049760f6bc3ca36e663fdb64d9e7f6f3
lotany <ablekilinda@gmail.com>
2024-11-26 05:44:18 +0300

    adding objects to classes

--- day21/classes.py
+++ day21/classes.py
@@ -3,5 +3,27 @@ class Person:
         self.firstname = firstname
         self.lastname = lastname
         self.country =country
+
+    def personal_info(self):
+        return print('My name is ',p.firstname, p.lastname, 'I am from ',p.country)
 p =Person('Lotan','Kilinda','Kenya')
-print('My name is ',p.firstname, p.lastname, 'I am from ',p.country)
+print(p.personal_info())
+
+#objects can have methods - methods are functions wich belong to the object
+
+#object default methods
+class Person:
+    def __init__(self,firstname='Tony',lastname='Kilinda',residence='Mombasa'):
+        self.firstname=firstname
+        self.lastname =lastname
+        self.residence=residence
+
+    def more_Details(self):
+        s='My name is, '
+        m = ' I am from '
+        p = print(s,self.firstname,self.lastname,m,self.residence)
+        return p
+    
+p =Person('Lotan','Kilinda','Mombasa')
+print(p.more_Details())
+        
