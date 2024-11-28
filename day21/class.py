5293a7349ae4d061da5beff453603c4e8b2ea16a
lotany <ablekilinda@gmail.com>
2024-11-28 05:53:14 +0300

    classess in poython

---
+++ day21/class.py
@@ -0,0 +1,10 @@
+print('Hello world!')
+name = input("Enter name: ")
+
+def name_List(last):
+    if name =="Lotan":
+        x =2024
+        y=1998
+        age = x-y
+        print(last, "He is ",age, "years old")
+name_List('Kilinda')
