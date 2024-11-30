6817588abe6780d4a9c54c89f1cd123fb6664035
lotany <ablekilinda@gmail.com>
2024-11-30 05:48:59 +0300

    largest number

--- day22/exer.py
+++ day22/exer.py
@@ -1,4 +1,12 @@
-x = int(input())
-y = int(input())
- 
-print(x + y)
+number1=int(input("Enter the first number: "))
+number2=int(input("Enter the second number: "))
+number3=int(input("Enter the third number: "))
+
+largest_no = number1
+if number2 >largest_no:
+    largest_no =number2
+    
+if number3>largest_no:
+    largest_no =number3
+
+print("The largest number is, ",largest_no)
