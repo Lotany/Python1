db817d8576fec1fed491ae5d9a6db7b8fb355a31
lotany <ablekilinda@gmail.com>
2024-12-02 22:38:38 +0300

    functions

---
+++ day23/cilla.py
@@ -0,0 +1,17 @@
+import datetime
+name =input('Enter your name: ')
+
+def run():
+    if name == 'lotan':
+        print('Run 10km')
+    else:
+        print('Reject reject')
+def count():
+    no =10
+    while no<=10:
+        no +=1
+        print(no)
+run()
+count()
+
+
